739ceb87a4e1779508c9b8bbc1f37aee49ad6456
Kevin Chan <chankevin13@gmail.com>
2023-02-15 16:37:01 -0500

    show

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
+  def show
+    render json: params
+  end
+
+  def create
+    render json: params
+  end
+
   def index
     render plain: "I'm in the index action!"
   end
+
 end
