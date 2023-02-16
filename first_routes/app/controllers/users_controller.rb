02f934e6519338428012b2d9c1f6c74fb54e600c
Sabrina Desmond <sabrinawdesmond@gmail.com>
2023-02-16 10:43:45 -0500

    fixed pt2

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -5,21 +5,16 @@ def index
   end
 
   def create
-    # render json: params
+
     user = User.new(user_params)
     if user.save
       render json: user
     else
       render json: user.errors.full_messages, status: :unprocessable_entity
-      # 422 is the status code for an unprocessable entity.
-      # You can either pass the status code or status symbol.
-      # In other words, you could also return:
-      # render json: user.errors.full_messages, status: 422
     end
   end
 
   def show
-    # render json: params
     render json: User.find(params[:id])
   end
 
