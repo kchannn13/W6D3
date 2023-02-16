6956cc9c1a0d561f481f7e1eb34c1b9934db7a72
Sabrina Desmond <sabrinawdesmond@gmail.com>
2023-02-16 11:46:33 -0500

    artwork unique

--- first_routes/app/models/artwork.rb
+++ first_routes/app/models/artwork.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :bigint           not null, primary key
+#  image_url  :string           not null
+#  title      :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  artist_id  :integer          not null
+#
+# Indexes
+#
+#  index_artworks_on_artist_id            (artist_id)
+#  index_artworks_on_artist_id_and_title  (artist_id,title) UNIQUE
+#
 class Artwork < ApplicationRecord
   validates :image_url, :artist_id, presence: true, uniqueness: {scope: :title}
 end
--- first_routes/db/schema.rb
+++ first_routes/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_16_162104) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_16_163622) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_16_162104) do
     t.integer "artist_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["artist_id", "title"], name: "index_artworks_on_artist_id_and_title", unique: true
     t.index ["artist_id"], name: "index_artworks_on_artist_id"
   end
 
