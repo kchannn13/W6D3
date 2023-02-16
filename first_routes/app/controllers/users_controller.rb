686a0498b75ed4b347f0c1011b4529bef91809c7
Sabrina Desmond <sabrinawdesmond@gmail.com>
2023-02-16 10:43:13 -0500

    fixed

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -1,21 +1,46 @@
 class UsersController < ApplicationController
-  def show
-    render json: params
+def index
+    # render plain: "I'm in the index action!"
+    render json: User.all
   end
 
   def create
-    user = User.new(params.require(:name).permit(:email))
-    user.save!
+    # render json: params
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+      # 422 is the status code for an unprocessable entity.
+      # You can either pass the status code or status symbol.
+      # In other words, you could also return:
+      # render json: user.errors.full_messages, status: 422
+    end
+  end
+
+  def show
+    # render json: params
+    render json: User.find(params[:id])
+  end
+
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
     render json: user
   end
 
-  def index
-    @users = User.all
-    render json: @users 
-  end
+  private
 
   def user_params
-    params.require(:name).permit(:email)
+    params.require(:user).permit(:name, :email)
   end
-
 end
